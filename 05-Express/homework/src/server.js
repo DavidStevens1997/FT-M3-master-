e65bcb3f7f2139d8572b9232c2495cbe9bc144fa
DavidStevens1997 <david.hr97110105243@gmail.com>
2022-11-09 17:25:58 -0500

    finish5hw

--- 05-Express/homework/src/server.js
+++ 05-Express/homework/src/server.js
@@ -5,7 +5,7 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+let posts = [];
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -99,4 +99,40 @@ server.put(PATH, (req, res) => {
     }
     res.send('done');
 });
+server.delete(PATH, (req, res) => {
+    let {id} = req.body;
+
+    const post = posts.find(p => p.id === parseInt(id));
+    if(!id || !post){
+        return res.status(STATUS_USER_ERROR).json({error: "Mensaje de error"});
+    }
+
+    posts = posts.filter(p => p.id !== parseInt(id));
+    res.json({ success: true});
+});
+
+server.delete('/author', (req, res) => {
+    let {author} = req.body;
+    const author_found = posts.find(p => p.author === author);
+    if (!author || !author_found) {
+        return res.status(STATUS_USER_ERROR).json({error: "No existe el autor indicado"});
+    }
+
+    let delete_authors = [];
+
+    //opcion1
+    //delete_authors = posts.filter(p => p.author === author);
+    //posts = posts.filter(p => p.author !== author);
+
+    //opcion2
+    posts = posts.filter (p => {
+        if(p.author !== author){
+            return true;
+        }else{
+            delete_authors.push(p);
+        }
+    });
+    return res.json(delete_authors);
+});
+
 module.exports = { posts, server };
